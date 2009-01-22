750a5c8227b06526873c03655f870b3386c82181
Igor Gladkoborodov <4layers@gmail.com>
2009-01-23 02:13:05 +0800

    add mime-type fixing
    Signed-off-by: Mikhail <tmp.over@gmail.com>

--- lib/swf_upload.rb
+++ lib/swf_upload.rb
@@ -1,5 +1,54 @@
 module SwfUpload
+
+  MIME_TYPES = {
+    "gif" => "image/gif",
+    "jpg" => "image/jpeg",
+    "jpeg" => "image/jpeg",
+    "png" => "image/png",
+    "swf" => "application/x-shockwave-flash",
+    "pdf" => "application/pdf",
+    "sig" => "application/pgp-signature",
+    "spl" => "application/futuresplash",
+    "doc" => "application/msword",
+    "ps" => "application/postscript",
+    "torrent" => "application/x-bittorrent",
+    "dvi" => "application/x-dvi",
+    "gz" => "application/x-gzip",
+    "pac" => "application/x-ns-proxy-autoconfig",
+    "swf" => "application/x-shockwave-flash",
+    "tar.gz" => "application/x-tgz",
+    "tar" => "application/x-tar",
+    "zip" => "application/zip",
+    "mp3" => "audio/mpeg",
+    "m3u" => "audio/x-mpegurl",
+    "wma" => "audio/x-ms-wma",
+    "wax" => "audio/x-ms-wax",
+    "wav" => "audio/x-wav",
+    "xbm" => "image/x-xbitmap",
+    "xpm" => "image/x-xpixmap",
+    "xwd" => "image/x-xwindowdump",
+    "css" => "text/css",
+    "html" => "text/html",
+    "js" => "text/javascript",
+    "txt" => "text/plain",
+    "xml" => "text/xml",
+    "mpeg" => "video/mpeg",
+    "mov" => "video/quicktime",
+    "avi" => "video/x-msvideo",
+    "asf" => "video/x-ms-asf",
+    "wmv" => "video/x-ms-wmv"
+  }
+
   def swf_upload_data
-    params[:Filedata]
+    @swf_upload_data ||= fix_mime_type(params[:Filedata])
+  end
+
+private
+
+  def fix_mime_type(data)
+    if !data.is_a?(File) && mime_type = MIME_TYPES[File.extname(data.original_filename).delete('.')]
+      data.content_type = mime_type
+    end
+    data
   end
 end
