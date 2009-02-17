ec77bfa51b04047e721deaaf57c64dbab7d27556
Boba Fat <ivan@workisfun.ru>
2009-02-17 22:00:56 +0800

    README => README.rdoc, fixed README, added register_javascript_expansion
    Signed-off-by: Mikhail <tmp.over@gmail.com>

--- init.rb
+++ init.rb
@@ -1,2 +1,4 @@
 ActionView::Base.send(:include, SwfUploadHelper)
 ActionController::Base.send(:include, SwfUpload)
+
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :swf_upload => %w(swfupload swfupload_plugin)
