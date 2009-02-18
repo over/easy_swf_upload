b86af11f80d1981d02cb485e7f4750813d5e190c
Boba Fat <ivan@workisfun.ru>
2009-02-18 23:22:31 +0800

    FlashUploader became class + some other changes enhancments + prototype 1.6.0.3 compitability
    Signed-off-by: Mikhail <tmp.over@gmail.com>

--- generators/easy_swf_upload/templates/swfupload_plugin.js
+++ generators/easy_swf_upload/templates/swfupload_plugin.js
@@ -1,34 +1,36 @@
 var SwfuCookie = {
 	set: function(name, value, daysToExpire) {
-		var expire = '';
+		var expire = ''
 		if (daysToExpire != undefined) {
-			var d = new Date();
-			d.setTime(d.getTime() + (86400000 * parseFloat(daysToExpire)));
-			expire = '; expires=' + d.toGMTString();
+			var d = new Date()
+			d.setTime(d.getTime() + (86400000 * parseFloat(daysToExpire)))
+			expire = '; expires=' + d.toGMTString()
 		}
-		return (document.cookie = escape(name) + '=' + escape(value || '') + expire);
+		return (document.cookie = escape(name) + '=' + escape(value || '') + expire)
 	},
 	get: function(name) {
-		var cookie = document.cookie.match(new RegExp('(^|;)\\s*' + escape(name) + '=([^;\\s]*)'));
-		return (cookie ? unescape(cookie[2]) : null);
+		var cookie = document.cookie.match(new RegExp('(^|;)\\s*' + escape(name) + '=([^;\\s]*)'))
+		return (cookie ? unescape(cookie[2]) : null)
 	},
 	erase: function(name) {
-		var cookie = Cookie.get(name) || true;
-		Cookie.set(name, '', -1);
-		return cookie;
+		var cookie = Cookie.get(name) || true
+		Cookie.set(name, '', -1)
+		return cookie
 	},
 	accept: function() {
 		if (typeof navigator.cookieEnabled == 'boolean') {
-			return navigator.cookieEnabled;
+			return navigator.cookieEnabled
 		}
-		Cookie.set('_test', '1');
-		return (Cookie.erase('_test') === '1');
+		Cookie.set('_test', '1')
+		return (Cookie.erase('_test') === '1')
 	}
 }
 
-var flashUploader = {
-	init: function(block, index) {
+var FlashUploader = Class.create({
+	initialize: function(block, index) {
+		this.id = 'fu_' + index
 		this.swfUploadBlock = block
+		this.container = block.down('.uploadContainer')
 		this.postParams = new Hash()
 
 		this.postParams.set('authenticity_token', this.swfUploadBlock.down('.token').innerHTML)
@@ -38,84 +40,89 @@ var flashUploader = {
 			upload_url: this.swfUploadBlock.down('.url').innerHTML,
 			post_params: this.postParams.toObject(),
 			use_query_string: true,
-			
-			file_size_limit : "40 MB",
-			file_types : this.swfUploadBlock.down('.filetypes').innerHTML,
-			file_types_description : "",
-			file_upload_limit : "0",
 
-			file_dialog_complete_handler: flashUploader.fileDialogComplete,
-			file_dialog_start_handler: flashUploader.selectFiles,
-			upload_start_handler: flashUploader.myUploadStart,
-			upload_progress_handler : flashUploader.uploadProgress,
-			upload_error_handler : flashUploader.uploadError,
-			upload_success_handler : flashUploader.uploadSuccess,
-			upload_complete_handler : flashUploader.uploadComplete,
-			button_placeholder_id : this.swfUploadBlock.down('.embedArea').down('div').readAttribute('id'),
+			file_size_limit: "40 MB",
+			file_types: this.swfUploadBlock.down('.filetypes').innerHTML,
+			file_types_description: "",
+			file_upload_limit: 0,
+
+			file_dialog_complete_handler: this.fileDialogComplete.bind(this),
+			file_dialog_start_handler: null,
+			upload_start_handler: null,
+			upload_progress_handler: this.uploadProgress.bind(this),
+			upload_error_handler: this.uploadError.bind(this),
+			upload_success_handler: this.uploadSuccess.bind(this),
+			upload_complete_handler: this.uploadComplete.bind(this),
+			button_placeholder_id: this.swfUploadBlock.down('.embedArea').down('div').readAttribute('id'),
 			button_width: 180,
 			button_height: 18,
-			button_text : '<span class="button">'+this.swfUploadBlock.down('.buttonText').innerHTML+'</span>',
-			button_text_style : '.button { '+ this.swfUploadBlock.down('.buttonStyle').innerHTML +' }',
+			button_text: '<span class="button">' + this.swfUploadBlock.down('.buttonText').innerHTML + '</span>',
+			button_text_style: '.button { '+ this.swfUploadBlock.down('.buttonStyle').innerHTML +' }',
 			button_text_top_padding: 0,
 			button_text_left_padding: 0,
 			button_window_mode: SWFUpload.WINDOW_MODE.TRANSPARENT,
 			button_cursor: SWFUpload.CURSOR.HAND,
 			button_action: (this.swfUploadBlock.down('.singleFile').innerHTML == 'true') ? SWFUpload.BUTTON_ACTION.SELECT_FILE : SWFUpload.BUTTON_ACTION.SELECT_FILES,
-			custom_settings : {
-					container : this.swfUploadBlock.down('.uploadContainer'),
-					current_file_index: 0,
-					filePrefix: index
-			},
-			flash_url : "/flash/swfupload.swf",
+			flash_url: "/flash/swfupload.swf",
 			debug: false
 		}
 
-		this.swfu = new SWFUpload(this.settings);
+		this.swfu = new SWFUpload(this.settings)
+		this.currentFileIndex = 0
 	},
-	
-	cancelQueue: function(id) {
 
+	cancelQueue: function(id) {
 	},
 
 	fileDialogComplete: function(filesSelected, filesQueued) {
-		for (i = 0; i < filesQueued; i++) {
-			var template = new Template('<li id="#{prefix}_file_#{id}"><div class="progress" style="width:1%"></div><h6>#{title}</h6></li>')
-			var show = {
-				title: this.getFile(i)["name"], 
-				id: (this.customSettings.current_file_index + this.getFile(i)["index"]),
-				prefix: this.customSettings.filePrefix
-			}
-			
-			this.customSettings.container.insert(template.evaluate(show))
-		}
-		this.customSettings.current_file_index += i
-		this.startUpload()
+		var template = new Template('<li id="#{id}"><div class="progress" style="width:1px"></div><h6>#{title}</h6></li>')
+
+		filesQueued.times(function(i) {
+			var file = this.swfu.getFile(i)
+			this.container.insert(template.evaluate({id: this.fileDomId(file, this.currentFileIndex), title: file.name.escapeHTML()}))
+		}.bind(this))
+		this.currentFileIndex += filesQueued
+
+		this.swfu.startUpload()
 	},
-	
+
 	uploadProgress: function(file, bytesLoaded, bytesTotal) {
-		$(this.customSettings.filePrefix + '_file_' + file["index"]).down('div').setStyle({width: (bytesLoaded * 100 / bytesTotal + '%')})
+		$(this.fileDomId(file)).down('.progress').setStyle({width: (bytesLoaded * 100 / bytesTotal + '%')})
 	},
-	
+
 	uploadError: function(file, errorCode, message) {
 		alert(message)
 	},
 	uploadSuccess: function(file, serverData) {
-		$(this.customSettings.filePrefix + '_file_'+file["index"]).down('div').setStyle({width: '100%'})
-		
-		$(this.customSettings.filePrefix + '_file_'+file["index"]).fade({ duration: 0.5, afterFinish: function() {
-			$(this.customSettings.filePrefix + '_file_'+file["index"]).remove()
-		}.bind(this) });
-		eval(serverData)
+		this.uploadProgress(file, 1, 1)
+
+		$(this.fileDomId(file)).fade({duration: 0.5, afterFinish: function(obj) {
+			obj.element.remove()
+		}.bind(this)})
+		try {
+			eval(serverData)
+		} catch (e) {
+			alert(e)
+		}
 	},
 	uploadComplete: function(file) {
-		if (this.getStats().files_queued > 0) {
-			this.startUpload()
+		if (this.swfu.getStats().files_queued > 0) {
+			this.swfu.startUpload()
 		}
+	},
+
+	fileDomId: function(file, reference){
+		return this.id + '_file_' + (file.index + (reference == undefined ? 0 : reference))
 	}
+})
+FlashUploader.init = function() {
+	$$('.swfUploadArea').each(function(element, index) {
+		if (!element.flashUploader) {
+			element.flashUploader = new FlashUploader(element, index)
+		}
+	})
 }
 
-Event.observe(window, 'load', function() {
-	$$('.swfUploadArea').each(function(element, index) {
-		flashUploader.init(element, index)
-	})
+Event.observe(window, 'load', function(){
+	FlashUploader.init()
 })
--- lib/swf_upload_helper.rb
+++ lib/swf_upload_helper.rb
@@ -1,19 +1,19 @@
 module SwfUploadHelper
   def swf_upload_area(title, options)
-    "<div class='swfUploadArea'>
-        <div class='buttonText' style='display:none'>#{title}</div>
-        <div class='url' style='display:none'>#{options[:url]}</div>
-        <div class='token' style='display:none'>#{form_authenticity_token}</div>
-        <div class='session_key' style='display:none'>#{ActionController::Base.session[0][:session_key]}</div>
-        <div class='session_id' style='display:none'>#{session.session_id}</div>
-        <div class='filetypes' style='display:none'>#{options[:filetypes]}</div>
-        <div class='buttonStyle' style='display:none'>#{options[:button_style]}</div>
-        <div class='singleFile' style='display:none'>#{options[:single_file] ? 'true' : 'false'}</div>
-        <div class='embedArea'>
-            <div id='swfUploadButton'></div>
+    %Q{<div class="swfUploadArea">
+        <div class="buttonText" style="display:none">#{title}</div>
+        <div class="url" style="display:none">#{options[:url]}</div>
+        <div class="token" style="display:none">#{form_authenticity_token}</div>
+        <div class="session_key" style="display:none">#{ActionController::Base.session[0][:session_key]}</div>
+        <div class="session_id" style="display:none">#{session.session_id}</div>
+        <div class="filetypes" style="display:none">#{options[:filetypes]}</div>
+        <div class="buttonStyle" style="display:none">#{options[:button_style]}</div>
+        <div class="singleFile" style="display:none">#{options[:single_file] ? 'true' : 'false'}</div>
+        <div class="embedArea">
+            <div id="swfUploadButton"></div>
         </div>
-        <ul class='uploadContainer'>
+        <ul class="uploadContainer">
         </ul>
-    </div>"
+    </div>}
   end
 end
