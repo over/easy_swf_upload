83601b8faaed2e7a77ea5716de057ead77f07b48
Boba Fat <ivan@workisfun.ru>
2009-05-15 02:32:45 +0800

    escape session key to prevent raising CGI::Session::CookieStore::TamperedWithCookie due to + converted to space
    Signed-off-by: Mikhail <tmp.over@gmail.com>

--- lib/swf_upload_helper.rb
+++ lib/swf_upload_helper.rb
@@ -4,7 +4,7 @@ module SwfUploadHelper
         <div class="buttonText" style="display:none">#{title}</div>
         <div class="url" style="display:none">#{options[:url]}</div>
         <div class="token" style="display:none">#{form_authenticity_token}</div>
-        <div class="session_key" style="display:none">#{ActionController::Base.session[0][:session_key]}</div>
+        <div class="session_key" style="display:none">#{CGI::escape(ActionController::Base.session[0][:session_key])}</div>
         <div class="session_id" style="display:none">#{session.session_id}</div>
         <div class="filetypes" style="display:none">#{options[:filetypes]}</div>
         <div class="buttonStyle" style="display:none">#{options[:button_style]}</div>
