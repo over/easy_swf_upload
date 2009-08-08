c9dbd04ecd664981af9b2ca906dc52e1bcdc4e49
Tabunov Mikhail <tmp.over@gmail.com>
2009-08-08 04:39:25 +0400

    Now compatible with rails 2.3. Found and fixed some bugs.

--- generators/easy_swf_upload/templates/cgi_session_hack.rb
+++ generators/easy_swf_upload/templates/cgi_session_hack.rb
@@ -1,24 +1,46 @@
-class CGI::Session
-  # allows getting session_id from query string if cookie_only is set to false
-  alias original_initialize initialize
+#todo: Check GEM version in generator, split to different files
 
-  def initialize(cgiwrapper, option = {})
-    unless option['cookie_only']
-      session_key = option['session_key'] || '_session_id'
+if RAILS_GEM_VERSION < '2.3.0'
+  class CGI::Session
+    alias original_initialize initialize
 
-      env_table = cgiwrapper.send(:env_table) #todo find correct way to do this
-      session_id = [
-        env_table['QUERY_STRING'],
-        env_table['REQUEST_URI'][/\?(.+)/, 1]
-      ].collect do |s|
-        s[/#{session_key}=([^&]{32,})/, 1] if s
-      end.compact.first
+    def initialize(cgiwrapper, option = {})
+      unless option['cookie_only']
+        session_key = option['session_key'] || '_session_id'
 
-      if session_id
-        cgiwrapper.instance_variable_set(:@cookies, {session_key => [CGI.unescape(session_id)]})
+        env_table = cgiwrapper.send(:env_table) #todo find correct way to do this
+        session_id = [
+          env_table['QUERY_STRING'],
+          env_table['REQUEST_URI'][/\?(.+)/, 1]
+        ].collect do |s|
+          s[/#{session_key}=([^&]{32,})/, 1] if s
+        end.compact.first
+
+        if session_id
+          cgiwrapper.instance_variable_set(:@cookies, {session_key => [CGI.unescape(session_id)]})
+        end
       end
+
+      original_initialize(cgiwrapper, option)
+    end
+  end
+else
+  require 'rack/utils'
+
+  class FlashSessionCookieMiddleware
+    def initialize(app, session_key = '_session_id')
+      @app = app
+      @session_key = session_key
     end
 
-    original_initialize(cgiwrapper, option)
+    def call(env)
+      if env['HTTP_USER_AGENT'] =~ /^(Adobe|Shockwave) Flash/
+        params = ::Rack::Utils.parse_query(env['QUERY_STRING'])
+        env['HTTP_COOKIE'] = [ @session_key, params[@session_key] ].join('=').freeze unless params[@session_key].nil?
+      end
+      @app.call(env)
+    end
   end
+
+  ActionController::Dispatcher.middleware.insert_before(ActionController::Base.session_store, FlashSessionCookieMiddleware, ActionController::Base.session_options[:session_key])
 end
--- lib/swf_upload_helper.rb
+++ lib/swf_upload_helper.rb
@@ -1,11 +1,13 @@
 module SwfUploadHelper
   def swf_upload_area(title, options)
+    session_key = RAILS_GEM_VERSION < "2.3.0" ? ActionController::Base.session[0][:session_key] : ActionController::Base.session_options[:session_key]
+    
     %Q{<div class="swfUploadArea">
         <div class="buttonText" style="display:none">#{title}</div>
         <div class="url" style="display:none">#{options[:url]}</div>
         <div class="token" style="display:none">#{form_authenticity_token}</div>
-        <div class="session_key" style="display:none">#{CGI::escape(ActionController::Base.session[0][:session_key])}</div>
-        <div class="session_id" style="display:none">#{session.session_id}</div>
+        <div class="session_key" style="display:none">#{CGI::escape(session_key)}</div>
+        <div class="session_id" style="display:none">#{cookies[session_key]}</div>
         <div class="filetypes" style="display:none">#{options[:filetypes]}</div>
         <div class="buttonStyle" style="display:none">#{options[:button_style]}</div>
         <div class="singleFile" style="display:none">#{options[:single_file] ? 'true' : 'false'}</div>
